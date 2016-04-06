4343ee18c07c46875543f2915e7e8f9ad575b3a7
Bart <b.erven@student.fontys.nl>
2016-04-06 13:52:01 +0200

    Recursing through the files

--- src/nl/fhict/pts4/Main.java
+++ src/nl/fhict/pts4/Main.java
@@ -1,8 +1,54 @@
 package nl.fhict.pts4;
 
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
+    public static void printFile(File fn) {
+
+        Photo photo = new Photo(fn);
+
+        try {
+            System.out.println(fn.getPath()+photo.getTitle());
+        } catch (IOException e) {
+            System.err.println("Can't read "+fn.getPath());
+            e.printStackTrace();
+        }
+
+    }
+
+    public static String PATH = "/mnt/iso";
+
+    public static List<Photo> photos = new ArrayList<>();
+
     public static void main(String[] args) {
+
+        File[] files = new File(PATH).listFiles();
+
+        // Loop through all files
+        for (File rootfile : files) {
+
+            // If a directory is in the root folder, enter it
+            if (rootfile.isDirectory()) {
+
+                // Create a login code
+                System.out.print("d ");
+
+                for (File folderfile : rootfile.listFiles())
+                {
+                    System.out.print("- ");
+                    printFile(folderfile);
+                }
+            }
+            else {
+                printFile(rootfile);
+            }
+        }
 
     }
 }
---
+++ src/nl/fhict/pts4/Photo.java
@@ -0,0 +1,80 @@
+package nl.fhict.pts4;
+
+import com.drew.imaging.ImageProcessingException;
+import com.drew.imaging.jpeg.JpegMetadataReader;
+import com.drew.metadata.*;
+import com.drew.metadata.iptc.IptcDirectory;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by before on 3/30/16.
+ */
+public class Photo {
+    private File file;
+    private File thumbnail;
+
+    public static final int THUMBNAIL_WIDTH = 300;
+    public static final int THUMBNAIL_HEIGHT = 300;
+
+    public static final String FILEPATH = "/tmp/dvds/";
+
+    Photo(File file) {
+        this.file = file;
+    }
+
+    public String getTitle() throws IOException {
+        String title = null;
+        try {
+            Metadata metadata = JpegMetadataReader.readMetadata(file);
+            Directory iptc = metadata.getFirstDirectoryOfType(IptcDirectory.class);
+
+            /*
+             * 632 = Caption/abstract
+             * 617 = Headline
+             * 517 = Object name
+             */
+            title = iptc.getString(617);
+            if (title == null) { title = iptc.getString(517);}
+            if (title == null) { title = iptc.getString(632);}
+        } catch (ImageProcessingException e) {
+            e.printStackTrace();
+        }
+
+        return title;
+    }
+
+    private String getPath(){
+        return file.getPath();
+    }
+
+    private String getResolution() throws IOException {
+        BufferedImage bimg = ImageIO.read(file);
+        int width          = bimg.getWidth();
+        int height         = bimg.getHeight();
+
+        return width + "x" + height;
+    }
+
+    private void createThumbnail() throws IOException {
+        BufferedImage bimg = ImageIO.read(file);
+
+        Image thumb;
+        thumb = bimg.getScaledInstance(THUMBNAIL_WIDTH, THUMBNAIL_HEIGHT, Image.SCALE_SMOOTH);
+
+        String folder = file.getParent();
+        String[] filename = file.getName().split(".");
+
+        this.thumbnail = new File("");
+    }
+
+    public File getThumbnail() throws IOException {
+        if (thumbnail == null)
+            createThumbnail();
+        return thumbnail;
+    }
+}
